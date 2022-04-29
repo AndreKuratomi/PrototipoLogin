61877cc10766b5384da345518e9f218a3fae421f
Andre <andrekuratomi@gmail.com>
2022-04-29 16:58:49 -0300

    Lib depcheck adicionada e animação para menu lateral rascunhada

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -6,7 +6,6 @@ import { DateTimeMoment } from "../../utils";
 
 import {
   AppBar,
-  Box,
   Button,
   Snackbar,
   Tab,
@@ -16,7 +15,7 @@ import {
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import MuiAlert from "@material-ui/lab/Alert";
-import { DateTimePicker } from "@material-ui/pickers";
+import { Box, FormControlLabel, Slide, Switch } from "@mui/material";
 
 import { A, LeftBar } from "./styles";
 
@@ -36,6 +35,7 @@ const useStyles = makeStyles((hide) => ({
     position: "absolute",
     zIndex: 0,
     width: "fit-content",
+    transition: "all .5s ease-in-out",
   },
   asideHeaderButton1: {
     backgroundColor: "#00f",
@@ -110,11 +110,9 @@ const Dashboard = () => {
 
   // MOSTRAR/ESCONDER ASIDEMENU:
   const [hide, setHide] = useState(false);
-  const openMenu = () => {
-    setHide(false);
-  };
-  const closeMenu = () => {
-    setHide(true);
+  const containerRef = useRef(null); //O QUE USEREF FAZ???
+  const handleMenu = () => {
+    setHide((prev) => !prev);
   };
 
   // STYLES:
@@ -143,12 +141,12 @@ const Dashboard = () => {
             <figcaption>Vestcasa</figcaption>
           </figure>
         </Box>
-        {hide === true ? (
+        {/* {hide === true ? (
           <Button
             className={classes.asideHeaderButton2}
             color="primary"
             variant="contained"
-            onClick={openMenu}
+            onClick={handleMenu}
           >
             <A>Menu</A>
           </Button>
@@ -157,11 +155,11 @@ const Dashboard = () => {
             className={classes.asideHeaderButton1}
             color="primary"
             variant="contained"
-            onClick={closeMenu}
+            onClick={handleMenu}
           >
             <A>Hide</A>
           </Button>
-        )}
+        )} */}
         <Box className={classes.userHeaderBox}>
           <p>{moment}</p>
           {/* <p>Quarta-feira, 27/04/2022 - 16:48:00</p> */}
@@ -178,30 +176,54 @@ const Dashboard = () => {
           </Button>
         </Box>
       </AppBar>
-      <LeftBar hide={hide}>
-        <AppBar className={classes.asideHeader}>
-          {/* <TextField
-            className={classes.textField}
-            label="Pesquisar"
-            variant="standard"
-          ></TextField> */}
-          <Tabs orientation="vertical">
-            {/* VERIFICAR CONSOLE.LOG! children?*/}
-            <Tab className={classes.tab} label="Item 1" />
-            <Tab className={classes.tab} label="Item 2" />
-            <Tab className={classes.tab} label="Item 3" />
-            <Tab className={classes.tab} label="Item 4" />
-            <Tab className={classes.tab} label="Item 5" />
-            <Tab className={classes.tab} label="Item 6" />
-            <Tab className={classes.tab} label="Item 7" />
-            <Tab className={classes.tab} label="Item 8" />
-            <Tab className={classes.tab} label="Item 9" />
-            <Tab className={classes.tab} label="Item 10" />
-            <Tab className={classes.tab} label="Item 11" />
-            <Tab className={classes.tab} label="Item 12" />
-          </Tabs>
-        </AppBar>
-      </LeftBar>
+      <Box
+        sx={{
+          // height: 180,
+          // width: 240,
+          // display: "flex",
+          // padding: 2,
+          // borderRadius: 1,
+          // bgcolor: (theme) =>
+          //   theme.palette.mode === "light" ? "grey.100" : "grey.900",
+          marginTop: "5rem",
+          overflow: "hidden",
+        }}
+        ref={containerRef}
+      >
+        <Box
+        //  sx={{ width: 200 }}
+        >
+          {/* <FormControlLabel
+            control={<Switch checked={hide} onChange={handleMenu} />}
+            label="Show from target"
+          // /> */}
+          <LeftBar
+            hide={hide}
+            // control={}
+          >
+            <Slide direction="right" in={hide} container={containerRef.current}>
+              {/* {icon} */}
+              <AppBar className={classes.asideHeader}>
+                <Tabs orientation="vertical" value={""}>
+                  {/* VERIFICAR CONSOLE.LOG! children?*/}
+                  <Tab className={classes.tab} label="Item 1" />
+                  <Tab className={classes.tab} label="Item 2" />
+                  <Tab className={classes.tab} label="Item 3" />
+                  <Tab className={classes.tab} label="Item 4" />
+                  <Tab className={classes.tab} label="Item 5" />
+                  <Tab className={classes.tab} label="Item 6" />
+                  <Tab className={classes.tab} label="Item 7" />
+                  <Tab className={classes.tab} label="Item 8" />
+                  <Tab className={classes.tab} label="Item 9" />
+                  <Tab className={classes.tab} label="Item 10" />
+                  <Tab className={classes.tab} label="Item 11" />
+                  <Tab className={classes.tab} label="Item 12" />
+                </Tabs>
+              </AppBar>
+            </Slide>
+          </LeftBar>
+        </Box>
+      </Box>
     </>
   );
 };
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -2,6 +2,64 @@ import moment from "moment";
 import { useState } from "react";
 import { Article } from "./styles";
 
+// export const DateTimeMoment = () => {
+//   const [value, setValue] = useState(new Date());
+
+//   setInterval(() => {
+//     setValue(new Date());
+//   }, 1000);
+
+//   let date = value;
+
+//   let displayYear = date.getFullYear();
+//   let displayMonth = date.getMonth() + 1; //?!?!?!??!
+//   let displayDay = date.getDate();
+
+//   if (displayMonth < 10) {
+//     displayMonth = "0" + displayMonth;
+//   }
+//   if (displayDay < 10) {
+//     displayDay = "0" + displayDay;
+//   }
+
+//   let fullDate = displayDay + "/" + displayMonth + "/" + displayYear;
+
+//   let displayHour = date.getHours();
+//   let displayMinutes = date.getMinutes();
+//   let displaySeconds = date.getSeconds();
+
+//   if (displayHour < 10) {
+//     displayHour = "0" + displayHour;
+//   }
+//   if (displayMinutes < 10) {
+//     displayMinutes = "0" + displayMinutes;
+//   }
+//   if (displaySeconds < 10) {
+//     displaySeconds = "0" + displaySeconds;
+//   }
+
+//   let fullTime = displayHour + ":" + displayMinutes + ":" + displaySeconds;
+
+//   const semana = [
+//     "Domingo",
+//     "Segunda-Feira",
+//     "Terça-Feira",
+//     "Quarta-Feira",
+//     "Quinta-Feira",
+//     "Sexta-Feira",
+//     "Sábado",
+//   ];
+
+//   return (
+//     <Article>
+//       <p>
+//         {semana[date.getDay()]}, {fullDate}
+//       </p>
+//       <p>{fullTime}</p>
+//     </Article>
+//   );
+// };
+
 export const DateTimeMoment = () => {
   const now = moment();
 
