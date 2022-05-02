5cc65403a79eb4669e39ee3e8519d5d01fae6da5
Andre <andrekuratomi@gmail.com>
2022-05-02 10:59:19 -0300

    fix: Tabs' value issue solved

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -115,6 +115,16 @@ const Dashboard = () => {
     setHide((prev) => !prev);
   };
 
+  // ASIDEMENU:
+  // const { value } = props;
+  const a11yProps = (index) => {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  };
+  const [value, setValue] = useState(0);
+
   // STYLES:
   const classes = useStyles(hide);
 
@@ -193,35 +203,46 @@ const Dashboard = () => {
         <Box
         //  sx={{ width: 200 }}
         >
-          {/* <FormControlLabel
-            control={<Switch checked={hide} onChange={handleMenu} />}
+          <FormControlLabel
             label="Show from target"
-          // /> */}
-          <LeftBar
+            control={<Switch checked={hide} onChange={handleMenu} />}
+          />
+          {/* <LeftBar
             hide={hide}
             // control={}
-          >
-            <Slide direction="right" in={hide} container={containerRef.current}>
-              {/* {icon} */}
-              <AppBar className={classes.asideHeader}>
-                <Tabs orientation="vertical" value={""}>
-                  {/* VERIFICAR CONSOLE.LOG! children?*/}
-                  <Tab className={classes.tab} label="Item 1" />
-                  <Tab className={classes.tab} label="Item 2" />
-                  <Tab className={classes.tab} label="Item 3" />
-                  <Tab className={classes.tab} label="Item 4" />
-                  <Tab className={classes.tab} label="Item 5" />
-                  <Tab className={classes.tab} label="Item 6" />
-                  <Tab className={classes.tab} label="Item 7" />
-                  <Tab className={classes.tab} label="Item 8" />
-                  <Tab className={classes.tab} label="Item 9" />
-                  <Tab className={classes.tab} label="Item 10" />
-                  <Tab className={classes.tab} label="Item 11" />
-                  <Tab className={classes.tab} label="Item 12" />
-                </Tabs>
-              </AppBar>
-            </Slide>
-          </LeftBar>
+          > */}
+          <Slide direction="right" in={hide} container={containerRef.current}>
+            {/* {icon} */}
+            <AppBar className={classes.asideHeader}>
+              <Tabs orientation="vertical" value={value}>
+                <Tab className={classes.tab} label="Item 1" {...a11yProps(0)} />
+                <Tab className={classes.tab} label="Item 2" {...a11yProps(1)} />
+                <Tab className={classes.tab} label="Item 3" {...a11yProps(2)} />
+                <Tab className={classes.tab} label="Item 4" {...a11yProps(3)} />
+                <Tab className={classes.tab} label="Item 5" {...a11yProps(4)} />
+                <Tab className={classes.tab} label="Item 6" {...a11yProps(5)} />
+                <Tab className={classes.tab} label="Item 7" {...a11yProps(6)} />
+                <Tab className={classes.tab} label="Item 8" {...a11yProps(7)} />
+                <Tab className={classes.tab} label="Item 9" {...a11yProps(8)} />
+                <Tab
+                  className={classes.tab}
+                  label="Item 10"
+                  {...a11yProps(9)}
+                />
+                <Tab
+                  className={classes.tab}
+                  label="Item 11"
+                  {...a11yProps(10)}
+                />
+                <Tab
+                  className={classes.tab}
+                  label="Item 12"
+                  {...a11yProps(11)}
+                />
+              </Tabs>
+            </AppBar>
+          </Slide>
+          {/* </LeftBar> */}
         </Box>
       </Box>
     </>
