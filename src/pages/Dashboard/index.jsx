2f5f35c210074ce01d870ed6b9cf556c83028588
Andre <andrekuratomi@gmail.com>
2022-05-03 17:11:20 -0300

    Dashboard's children elements drafted

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -21,9 +21,9 @@ import { Box, FormControlLabel, Slide, Switch } from "@mui/material";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 
-import { A, LeftBar } from "./styles";
+import { A } from "./styles";
 
-const useStyles = makeStyles((hide) => ({
+const useStyles = makeStyles(({ hide }) => ({
   topHeader: {
     display: "flex",
     flexDirection: "row",
@@ -36,16 +36,36 @@ const useStyles = makeStyles((hide) => ({
       padding: "0 0.5rem",
     },
   },
-  asideHeader: {
+  asideHeaderOpened: {
     backgroundColor: "#009E4F",
-    // marginTop: "4.3rem",
     right: "auto",
     position: "relative",
     zIndex: 2,
-    width: "fit-content",
-    transition: "all .5s ease-in-out",
-    "@media (min-width: 768px)": {
-      // marginTop: "5.75rem",
+    width: "4.5rem",
+    // width: hide ? 0 : ,
+    transition: "all .7s ease-in-out",
+  },
+  asideHeaderClosed: {
+    backgroundColor: "#009E4F",
+    right: "auto",
+    // position: "relative",
+    zIndex: 2,
+    width: 0,
+    // width: hide ? 0 : ,
+    transition: "all .7s ease-in-out",
+  },
+  asideHeaderButtonClosed: {
+    color: "#000",
+    backgroundColor: "#FFF",
+    borderRadius: "1rem",
+    position: "absolute",
+    // left: hide ? 0 : "3.4rem",
+    left: 0,
+    top: "4.65rem",
+    // transition: "all .4s ease-in-out",
+    zIndex: 3,
+    "@media (min-width: 383px)": {
+      top: "3.5rem",
     },
   },
   asideHeaderButtonOpened: {
@@ -53,32 +73,18 @@ const useStyles = makeStyles((hide) => ({
     backgroundColor: "#FFF",
     borderRadius: "1rem",
     position: "absolute",
-    left: 0,
-    top: "5rem",
-    transition: "all .4s ease-in-out",
+    // left: hide ? 0 : "3.4rem",
+    left: "3.4rem",
+    top: "4.65rem",
+    // transition: "all .4s ease-in-out",
     zIndex: 3,
-    "@media (min-width: 600px)": {
-      top: "3.5rem",
-    },
-  },
-  asideHeaderButtonClosed: {
-    color: "#000",
-    backgroundColor: "#FFF",
-    borderRadius: "1rem",
-    position: "absolute",
-    left: "5.1rem",
-    top: "5rem",
-    transition: "all .5s ease-in-out",
-    zIndex: 3,
-    "@media (min-width: 600px)": {
-      left: "9.1rem",
+    "@media (min-width: 383px)": {
       top: "3.5rem",
     },
   },
   tab: {
     "& .MuiTab-wrapper": {
       alignItems: "start",
-      marginLeft: "1rem",
     },
   },
   userHeaderBox: {
@@ -94,12 +100,87 @@ const useStyles = makeStyles((hide) => ({
       marginRight: "0.5rem",
     },
   },
-  boxDocuments: {
-    backgroundColor: "#aaa",
-    width: "77vw",
-    height: "77vw",
+  boxDocumentsWithAsideOpened: {
+    // backgroundColor: hide ? "#f00" : "#aaa",
+    width: "95vw",
+    // width: hide ? "100vw" : "95vw",
+    height: "576px",
+    display: "flex",
+    flexWrap: "wrap",
     position: "relative",
     zIndex: 1,
+  },
+  boxDocumentsWithAsideClosed: {
+    // backgroundColor: hide ? "#f00" : "#aaa",
+    width: "100vw",
+    // width: hide ? "100vw" : "95vw",
+    height: "576px",
+    position: "relative",
+    zIndex: 1,
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  boxMenuWAMClosed: {
+    backgroundColor: "#f0f",
+    borderRadius: "1rem",
+    width: "100vw",
+    // width: hide ? "100vw" : "90vw",
+    height: "10vh",
+    margin: "1rem",
+  },
+  boxMenuWAMOpened: {
+    backgroundColor: "#f0f",
+    borderRadius: "1rem",
+    width: "90vw",
+    // width: hide ? "100vw" : "90vw",
+    height: "10vh",
+    margin: "1rem",
+  },
+  boxGraphicWAMClosed: {
+    backgroundColor: "#ff0",
+    borderRadius: "1rem",
+    width: "100vw",
+    // width: hide ? "100vw" : "90vw",
+    height: "40vh",
+    margin: "1rem",
+  },
+  boxGraphicWAMOpened: {
+    backgroundColor: "#ff0",
+    borderRadius: "1rem",
+    width: "90vw",
+    // width: hide ? "100vw" : "90vw",
+    height: "40vh",
+    margin: "1rem",
+  },
+  boxTopWAMClosed: {
+    backgroundColor: "#00f",
+    borderRadius: "1rem",
+    width: "45vw",
+    height: "20vh",
+    margin: "1rem",
+  },
+  boxTopWAMOpened: {
+    backgroundColor: "#00f",
+    borderRadius: "1rem",
+    width: "39vw",
+    height: "20vh",
+    margin: "1rem",
+  },
+  boxVendasSemanaWAMClosed: {
+    backgroundColor: "#0ff",
+    borderRadius: "1rem",
+    width: "45vw",
+    height: "20vh",
+    margin: "1rem",
+  },
+  boxVendasSemanaWAMOpened: {
+    backgroundColor: "#0ff",
+    borderRadius: "1rem",
+    width: "39vw",
+    height: "20vh",
+    margin: "1rem",
   },
 }));
 
@@ -149,6 +230,7 @@ const Dashboard = () => {
 
   // STYLES:
   const classes = useStyles(hide);
+  // console.log(classes.asideHeaderButton.top);
 
   return (
     <>
@@ -191,198 +273,193 @@ const Dashboard = () => {
       <Box sx={{ display: "flex", width: "100vw" }}>
         {!!hide ? (
           <>
-            <Box
-              sx={{
-                // overflow: "hidden",
-                width: "23vw",
-              }}
-              // ref={containerRef}
-            >
-              <Box>
-                <Slide
-                  direction="right"
-                  in={hide}
-                  container={containerRef.current}
-                >
-                  <ArrowCircleLeftIcon
-                    className={classes.asideHeaderButtonClosed}
-                    onClick={handleMenu}
-                  />
-                </Slide>
-                {/* <ArrowCircleRightIcon
-                className={classes.asideHeaderButtonOpened}
-                onClick={handleMenu}
-              />
-            )} */}
-                <Slide
-                  direction="right"
-                  in={hide}
-                  container={containerRef.current}
-                >
-                  <AppBar className={classes.asideHeader}>
-                    <Tabs orientation="vertical" value={value}>
-                      <Tab
-                        className={classes.tab}
-                        label="Item 1"
-                        {...a11yProps(0)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 2"
-                        {...a11yProps(1)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 3"
-                        {...a11yProps(2)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 4"
-                        {...a11yProps(3)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 5"
-                        {...a11yProps(4)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 6"
-                        {...a11yProps(5)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 7"
-                        {...a11yProps(6)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 8"
-                        {...a11yProps(7)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 9"
-                        {...a11yProps(8)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 10"
-                        {...a11yProps(9)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 11"
-                        {...a11yProps(10)}
-                      />
-                      <Tab
-                        className={classes.tab}
-                        label="Item 12"
-                        {...a11yProps(11)}
-                      />
-                    </Tabs>
-                  </AppBar>
-                </Slide>
-              </Box>
+            {/* <Box> */}
+            <Box>
+              <Slide
+                direction="right"
+                in={hide}
+                container={containerRef.current}
+              >
+                <ArrowCircleLeftIcon
+                  className={classes.asideHeaderButtonOpened}
+                  onClick={handleMenu}
+                />
+              </Slide>
+              <Slide
+                direction="right"
+                in={hide}
+                container={containerRef.current}
+              >
+                <AppBar className={classes.asideHeaderOpened}>
+                  <Tabs
+                    orientation="vertical"
+                    value={value}
+                    sx={{ width: "5rem" }}
+                  >
+                    <Tab
+                      className={classes.tab}
+                      label="Item 1"
+                      {...a11yProps(0)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 2"
+                      {...a11yProps(1)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 3"
+                      {...a11yProps(2)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 4"
+                      {...a11yProps(3)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 5"
+                      {...a11yProps(4)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 6"
+                      {...a11yProps(5)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 7"
+                      {...a11yProps(6)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 8"
+                      {...a11yProps(7)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 9"
+                      {...a11yProps(8)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 10"
+                      {...a11yProps(9)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 11"
+                      {...a11yProps(10)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 12"
+                      {...a11yProps(11)}
+                    />
+                  </Tabs>
+                </AppBar>
+              </Slide>
             </Box>
-            <Slide direction="left" in={hide} container={containerRef.current}>
-              <Box sx={{ backGroundColor: "#0F0", width: "77vw" }}>
-                OI
-                <Box className={classes.boxDocuments}>1</Box>
-                <Box className={classes.boxDocuments}>2</Box>
-                <Box className={classes.boxDocuments}>3</Box>
-                <Box className={classes.boxDocuments}>4</Box>
-                <Box className={classes.boxDocuments}>5</Box>
-              </Box>
-            </Slide>
+            {/* </Box> */}
+            {/* <Slide direction="left" in={hide} container={containerRef.current}> */}
+            <Box className={classes.boxDocumentsWithAsideOpened}>
+              <Box className={classes.boxMenuWAMOpened}>1</Box>
+              <Box className={classes.boxGraphicWAMOpened}>2</Box>
+              <Box className={classes.boxTopWAMOpened}>3</Box>
+              <Box className={classes.boxVendasSemanaWAMOpened}>4</Box>
+              {/* <Box className={classes.box}>5</Box> */}
+            </Box>
+            {/* </Slide> */}
           </>
         ) : (
           <>
-            {/* <Slide direction="right" in={hide} container={containerRef.current}> */}
-            {/* ELABORAR UM FOR QUE REDUZA O NÚMERO DA LARGURA QUE NEM NO EXERCÍCIO DO CHATBOT*/}
-            <ArrowCircleRightIcon
-              className={classes.asideHeaderButtonOpened}
-              onClick={handleMenu}
-            />
-            {/* </Slide> */}
+            <Box>
+              {/* ELABORAR UM FOR QUE REDUZA O NÚMERO DA LARGURA QUE NEM NO EXERCÍCIO DO CHATBOT*/}
+              <ArrowCircleRightIcon
+                className={classes.asideHeaderButtonClosed}
+                onClick={handleMenu}
+              />
 
-            <Slide direction="right" in={hide} container={containerRef.current}>
-              <AppBar className={classes.asideHeader}>
-                <Tabs orientation="vertical" value={value}>
-                  <Tab
-                    className={classes.tab}
-                    label="Item 1"
-                    {...a11yProps(0)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 2"
-                    {...a11yProps(1)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 3"
-                    {...a11yProps(2)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 4"
-                    {...a11yProps(3)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 5"
-                    {...a11yProps(4)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 6"
-                    {...a11yProps(5)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 7"
-                    {...a11yProps(6)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 8"
-                    {...a11yProps(7)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 9"
-                    {...a11yProps(8)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 10"
-                    {...a11yProps(9)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 11"
-                    {...a11yProps(10)}
-                  />
-                  <Tab
-                    className={classes.tab}
-                    label="Item 12"
-                    {...a11yProps(11)}
-                  />
-                </Tabs>
-              </AppBar>
-            </Slide>
-            {/* <Slide direction="left" in={hide} container={containerRef.current}> */}
-            <Box sx={{ width: "100vw" }}>
-              TCHAU
-              <Box className={classes.boxDocuments}>1</Box>
-              <Box className={classes.boxDocuments}>2</Box>
-              <Box className={classes.boxDocuments}>3</Box>
-              <Box className={classes.boxDocuments}>4</Box>
-              <Box className={classes.boxDocuments}>5</Box>
+              <Slide
+                direction="right"
+                in={hide}
+                container={containerRef.current}
+              >
+                <AppBar className={classes.asideHeaderClosed} sx={{ width: 0 }}>
+                  <Tabs orientation="vertical" value={value}>
+                    <Tab
+                      className={classes.tab}
+                      label="Item 1"
+                      {...a11yProps(0)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 2"
+                      {...a11yProps(1)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 3"
+                      {...a11yProps(2)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 4"
+                      {...a11yProps(3)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 5"
+                      {...a11yProps(4)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 6"
+                      {...a11yProps(5)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 7"
+                      {...a11yProps(6)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 8"
+                      {...a11yProps(7)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 9"
+                      {...a11yProps(8)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 10"
+                      {...a11yProps(9)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 11"
+                      {...a11yProps(10)}
+                    />
+                    <Tab
+                      className={classes.tab}
+                      label="Item 12"
+                      {...a11yProps(11)}
+                    />
+                  </Tabs>
+                </AppBar>
+              </Slide>
+              {/* <Slide direction="left" in={hide} container={containerRef.current}> */}
+              <Box className={classes.boxDocumentsWithAsideClosed}>
+                <Box className={classes.boxMenuWAMClosed}>1</Box>
+                <Box className={classes.boxGraphicWAMClosed}>2</Box>
+                <Box className={classes.boxTopWAMClosed}>3</Box>
+                <Box className={classes.boxVendasSemanaWAMClosed}>4</Box>
+                {/* <Box className={classes.box}>5</Box> */}
+              </Box>
+              {/* </Slide> */}
             </Box>
-            {/* </Slide> */}
           </>
         )}
       </Box>
