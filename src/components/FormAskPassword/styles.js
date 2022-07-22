e6cbd63dc7969f628d2a36de9c714b06da125247
Andre <andrekuratomi@gmail.com>
2022-07-22 10:06:09 -0300

    feat(components/FormAskPassword): refatoração layout formulário pedido alteração de senha

--- src/components/FormAskPassword/styles.js
+++ src/components/FormAskPassword/styles.js
@@ -8,3 +8,13 @@ export const Article = styled.article`
     margin-top: 5rem;
   }
 `;
+
+export const A = styled.a`
+  color: #fff;
+  font-weight: 500;
+
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
