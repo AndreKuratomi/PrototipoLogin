11217f84b15527587e84b5da23bdbe724b52de3e
Andre <andrekuratomi@gmail.com>
2022-08-26 16:57:59 -0300

    style: últimos retoques para modal logout.

--- src/components/ModalLeaveAsk/index.tsx
+++ src/components/ModalLeaveAsk/index.tsx
@@ -4,9 +4,46 @@ import { Button, Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles((open) => ({
+  buttonBack: {
+    color: "var(--externalDashboardGreen)",
+    "&:hover": {
+      backgroundColor: "var(--white)",
+    },
+  },
+  buttonLeave: {
+    backgroundColor: "var(--externalDashboardGreen)",
+    color: "var(--white)",
+    filter: "drop-shadow(0.7rem 0.7rem 0.1rem rgba(3,3,3,8%))",
+    fontSize: "1rem",
+    marginBottom: "1rem",
+    "&:hover": {
+      backgroundColor: "var(--hoverGreen)",
+    },
+  },
   paper: {
-    display: "block",
+    // display: "block",
     // display: ${(props) => props.open ? (display: "block") : (display: "none")},
+    color: "var(--externalDashboardGreen)",
+    display: "flex",
+    flexDirection: "column",
+    height: "50vh",
+    minWidth: "90vw",
+    maxWidth: "90vw",
+    padding: "1rem",
+    textAlign: "center",
+    "@media (min-width: 768px)": {
+      minWidth: "30vw",
+
+      width: "30vw",
+    },
+  },
+  typo: {
+    fontSize: "2rem",
+    fontWeight: 500,
+    marginBottom: "2rem",
+    "@media (min-width: 768px)": {
+      fontSize: "2.5rem",
+    },
   },
 }));
 
@@ -26,9 +63,21 @@ export const ModalLeaveAsk = ({ open }: any) => {
 
   return (
     <Paper className={classes.paper}>
-      <Typography>Tem certeza que deseja sair?</Typography>
-      <Button onClick={clearLocalStorage}>Sair</Button>
-      <Button onClick={() => setOpen(false)}>Voltar</Button>
+      <Typography className={classes.typo}>
+        Tem certeza que deseja sair?
+      </Typography>
+
+      <Button
+        className={classes.buttonLeave}
+        color="secondary"
+        onClick={clearLocalStorage}
+        variant="contained"
+      >
+        Sair
+      </Button>
+      <Button className={classes.buttonBack} onClick={() => setOpen(false)}>
+        Voltar
+      </Button>
     </Paper>
   );
 };
