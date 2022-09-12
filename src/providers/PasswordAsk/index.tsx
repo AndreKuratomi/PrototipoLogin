e7d54e64749a67844eba0b4f24607b57e8e4c856
Andre <andrekuratomi@gmail.com>
2022-09-12 10:38:09 -0300

    feat: Conexão com API para troca de senha rascunhada.

--- src/providers/PasswordAsk/index.tsx
+++ src/providers/PasswordAsk/index.tsx
@@ -19,13 +19,10 @@ import {
 
 import bcrypt from "bcryptjs";
 
-import { send } from "emailjs-com";
-
-import { useToast } from "@chakra-ui/react";
-
 interface IAskProvider {
+  createAuth: () => void;
+  LoadPage: () => void;
   loading: boolean;
-  onSubmit: (form: any, e: any) => Promise<void>;
   setLoading: Dispatch<SetStateAction<boolean>>;
 }
 
@@ -43,28 +40,28 @@ export const PasswordAskProvider = ({ children }: IAskProviderProps) => {
     setLoading(true);
   };
 
-  // TOASTS:
-  const toast = useToast();
+  // // TOASTS:
+  // const toast = useToast();
 
-  const addSuccessToast = () => {
-    toast({
-      description: "Confira sua caixa de emails.",
-      duration: 5000,
-      position: "top",
-      status: "success",
-      title: "Solicitação enviada com sucesso!",
-    });
-  };
-  const addFailToast = () => {
-    toast({
-      description:
-        "Algo deu errado! Verifique se os dados preenchidos estão corretos ou se o email está cadastrado",
-      duration: 5000,
-      position: "top",
-      status: "error",
-      title: "Falha na solicitação!",
-    });
-  };
+  // const addSuccessToast = () => {
+  //   toast({
+  //     description: "Confira sua caixa de emails.",
+  //     duration: 5000,
+  //     position: "top",
+  //     status: "success",
+  //     title: "Solicitação enviada com sucesso!",
+  //   });
+  // };
+  // const addFailToast = () => {
+  //   toast({
+  //     description:
+  //       "Algo deu errado! Verifique se os dados preenchidos estão corretos ou se o email está cadastrado",
+  //     duration: 5000,
+  //     position: "top",
+  //     status: "error",
+  //     title: "Falha na solicitação!",
+  //   });
+  // };
 
   // GERAÇÂO DE 'TOKEN' E ALOCAÇÃO NO LOCALSTORAGE:
   const createAuth = () => {
@@ -72,63 +69,62 @@ export const PasswordAskProvider = ({ children }: IAskProviderProps) => {
     localStorage.setItem("@token: NewEmailToken", JSON.stringify(cryptoken));
   };
 
-  // VARIÁVEL USENAVIGATE:
-  const navigate = useNavigate();
+  // // VARIÁVEL SENHA PROVISÓRIA:
+  // const reducedUUID = uuidv4().substring(0, 13);
 
-  // VARIÁVEL SENHA PROVISÓRIA:
-  const reducedUUID = uuidv4().substring(0, 13);
-
-  // FORMATAÇÃO DE DATA:
-  const rawDate = new Date();
-  const date0 = rawDate.toLocaleString("pt-BR").split(" ")[0]; // dd/mm/aaaa
-  const date1 = rawDate.toLocaleString("pt-BR").split(" ")[1]; // hh:mm:ss
+  // // FORMATAÇÃO DE DATA:
+  // const rawDate = new Date();
+  // const date0 = rawDate.toLocaleString("pt-BR").split(" ")[0]; // dd/mm/aaaa
+  // const date1 = rawDate.toLocaleString("pt-BR").split(" ")[1]; // hh:mm:ss
 
   // ENVIO DE EMAIL:
-  let qwerty = {
-    date0: date0,
-    date1: date1,
-    email: "",
-    link: "http://localhost:3000/changepassword",
-    nova_senha: "",
-    random_password: reducedUUID,
-    reply_to: "suporte.vestcasa@gmail.com",
-    usuario: "",
-  };
+  // let qwerty = {
+  //   date0: date0,
+  //   date1: date1,
+  //   email: "",
+  //   link: "http://localhost:3000/changepassword",
+  //   nova_senha: "",
+  //   random_password: reducedUUID,
+  //   reply_to: "suporte.vestcasa@gmail.com",
+  //   usuario: "",
+  // };
 
-  const onSubmit = async (
-    form: { email: string; repeatNewPassword: any; usuario: string },
-    e: { preventDefault: () => void }
-  ) => {
-    LoadPage();
+  // const onSubmit = async (
+  //   form: { email: string; repeatNewPassword: any; usuario: string },
+  //   e: { preventDefault: () => void }
+  // ) => {
+  //   LoadPage();
 
-    qwerty.email = form.email;
-    qwerty.nova_senha = form.repeatNewPassword;
-    qwerty.usuario = form.usuario;
+  //   qwerty.email = form.email;
+  //   qwerty.nova_senha = form.repeatNewPassword;
+  //   qwerty.usuario = form.usuario;
 
-    e.preventDefault();
+  //   e.preventDefault();
 
-    await send(
-      "service_j5y5zw8",
-      "template_qya1x9k",
-      qwerty,
-      "AP4ks7G3vrdRa8AWJ"
-    )
-      .then((response) => {
-        addSuccessToast();
-        console.log("Email enviado!", response.status, response.text);
-        createAuth();
-        navigate("/");
-        setLoading(false);
-      })
-      .catch((err) => {
-        addFailToast();
-        console.log("Algo deu errado!", err);
-        setLoading(false);
-      });
-  };
+  //   await send(
+  //     "service_j5y5zw8",
+  //     "template_qya1x9k",
+  //     qwerty,
+  //     "AP4ks7G3vrdRa8AWJ"
+  //   )
+  //     .then((response) => {
+  //       addSuccessToast();
+  //       console.log("Email enviado!", response.status, response.text);
+  //       createAuth();
+  //       navigate("/");
+  //       setLoading(false);
+  //     })
+  //     .catch((err) => {
+  //       addFailToast();
+  //       console.log("Algo deu errado!", err);
+  //       setLoading(false);
+  //     });
+  // };
 
   return (
-    <PasswordAskContext.Provider value={{ onSubmit, loading, setLoading }}>
+    <PasswordAskContext.Provider
+      value={{ createAuth, loading, setLoading, LoadPage }}
+    >
       {children}
     </PasswordAskContext.Provider>
   );
