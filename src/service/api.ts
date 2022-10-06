f78b07437fa18b963d5a45ab4fefec925d24a17e
André Kuratomi <andrekuratomi@gmail.com>
2022-10-06 18:57:20 -0300

    feat: Comando tecla F12 reabilitada.

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
-  // baseURL: "http://ec2-23-23-27-9.compute-1.amazonaws.com:8000/api",
+  // baseURL: "http://dev-bi-abkura.s3-website-us-east-1.amazonaws.com:8000/api",
 });
 
 export default api;
